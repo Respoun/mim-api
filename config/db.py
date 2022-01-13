adc41516e86a88137cd40c8967140e293670aa48
guillaume <guillaume@ctis-MacBook-Pro.local>
2022-01-13 09:42:44 +0100

    add changes

--- config/db.py
+++ config/db.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, MetaData
 
-engine = create_engine("mysql+pymysql://ulx22cmrwgx8dthz:mRqUyxDYzG76k9TF1xG4@b4wdsgjk77puqlgcampo-mysql.services.clever-cloud.com:3306/b4wdsgjk77puqlgcampo")
+engine = create_engine("")
 
 meta = MetaData()
 conn = engine.connect()
